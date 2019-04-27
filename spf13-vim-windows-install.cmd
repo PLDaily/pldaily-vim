f5ae9de5f95ead1b8af771cb56c6ddbfce7eec02
PLDaily <pcd12321@gmail.com>
2019-04-27 11:55:34 +0800

    feat: support typescript

--- spf13-vim-windows-install.cmd
+++ spf13-vim-windows-install.cmd
@@ -18,7 +18,7 @@ REM    limitations under the License.
 
 @set APP_PATH=%HOME%\.spf13-vim-3
 IF NOT EXIST "%APP_PATH%" (
-    call git clone -b 3.0 https://github.com/spf13/spf13-vim.git "%APP_PATH%"
+    call git clone -b 3.0 https://github.com/pldaily/spf13-vim.git "%APP_PATH%"
 ) ELSE (
     @set ORIGINAL_DIR=%CD%
     echo updating spf13-vim
